dc3b57d574ea4664ad474f2e8b096e24e783293e
Emerson Hum <emersonhum@gmail.com>
2017-01-05 16:51:03 -0800

    Create first test

--- test/test.js
+++ test/test.js
@@ -1,2 +1,16 @@
 var chai = require('chai');
-var expect = chai.expect, assert = chai.assert, should = chai.should();
+var expect = chai.expect, 
+    assert = chai.assert, 
+    should = chai.should();
+var supertest = require('supertest');
+var api = supertest('http://localhost:3000');
+
+describe('Server', function() {
+  describe('Initialization', function() {
+    it('should return 200 response at /', function(done) {
+      api.get('/')
+      .set('Accept', 'application/json')
+      .expect(200, done);
+    });
+  });
+});
