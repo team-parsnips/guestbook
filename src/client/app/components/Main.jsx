188904a71c9a0cc0f6842960085526e78f5eaf0e
Rob0h <robert.is.oh@gmail.com>
2017-01-05 23:46:06 -0800

    Add material-ui

--- src/client/app/components/Main.jsx
+++ src/client/app/components/Main.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {deepOrange500} from 'material-ui/styles/colors';
+import {Card, CardHeader} from 'material-ui/Card';
+
+const muiTheme = getMuiTheme({
+  palette: { accent1Color: deepOrange500 }
+});
 
 class Main extends React.Component {
   render() {
     return (
-      <p> Guestbook Time </p>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Card>
+          <CardHeader
+            title="Guestbook" />
+        </Card>
+      </MuiThemeProvider>
     )
   }
 }
