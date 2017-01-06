1ce4973cb3d730328288c26d76f697df8419f449
Rob0h <robert.is.oh@gmail.com>
2017-01-06 15:29:47 -0800

    Create stubbed data

---
+++ src/server/exampleData/exampleProperty.js
@@ -0,0 +1,6 @@
+[{"name":"Hacker House","location":"944 Market Street, San Francisco, CA","check_in":"15:00:00 GMT-0800 (PST)","check_out":"11:00:00 GMT-0800 (PST)"},
+{"name":"SF Apartment","location":"429 Stockton St, San Francisco, CA 94108","check_in":"15:00:00 GMT-0800 (PST)","check_out":"11:00:00 GMT-0800 (PST)"},
+{"name":"Private Bedroom","location":"1 Front St #100, San Francisco, CA 94111","check_in":"15:00:00 GMT-0800 (PST)","check_out":"11:00:00 GMT-0800 (PST)"},
+{"name":"Twin Bed in Shared Room","location":"986 Market St, San Francisco, CA 94102","check_in":"15:00:00 GMT-0800 (PST)","check_out":"11:00:00 GMT-0800 (PST)"},
+{"name":"Private Room in Youth Hostel","location":"998 Market St, San Francisco, CA 94102","check_in":"15:00:00 GMT-0800 (PST)","check_out":"11:00:00 GMT-0800 (PST)"},
+{"name":"Rare Loft in Victorian House","location":"307 Kearny St, San Francisco, CA 94108","check_in":"15:00:00 GMT-0800 (PST)","check_out":"11:00:00 GMT-0800 (PST)"}
