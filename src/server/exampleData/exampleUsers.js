b0351b8ef8df7eaf5ddeec7ec338e1cc5cdacc5d
Rob0h <robert.is.oh@gmail.com>
2017-01-06 16:25:14 -0800

    Add bias to example data

--- src/server/exampleData/exampleUsers.js
+++ src/server/exampleData/exampleUsers.js
@@ -1,3 +1,3 @@
-[{"email":"jscott0@ted.com","password":"eAiDW1utq6r","first_name":"Joyce","last_name":"Scott"},
-{"email":"ajames1@usatoday.com","password":"MElaT7p","first_name":"Anthony","last_name":"James"},
-{"email":"rmurray2@drupal.org","password":"uLJ3Y5N6X3na","first_name":"Ronald","last_name":"Murray"}]
+[{"email":"jscott0@ted.com","password":"eAiDW1utq6r","firstName":"Joyce","lastName":"Scott"},
+{"email":"ajames1@usatoday.com","password":"MElaT7p","firstName":"Anthony","lastName":"James"},
+{"email":"rmurray2@drupal.org","password":"uLJ3Y5N6X3na","firstName":"Ronald","lastName":"Murray"}]
