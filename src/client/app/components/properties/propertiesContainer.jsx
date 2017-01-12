218ef91ab8e8909ee1dcc452be0df10647fecddf
Rob0h <robert.is.oh@gmail.com>
2017-01-11 22:51:11 -0800

    Create generateQRCode and render QR Code on client

--- src/client/app/components/properties/propertiesContainer.jsx
+++ src/client/app/components/properties/propertiesContainer.jsx
@@ -8,7 +8,6 @@ import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'mat
 import AddIcon from 'material-ui/svg-icons/content/add-circle';
 import RaisedButton from 'material-ui/RaisedButton';
 
-
 import PropertyList from './propertyList.jsx';
 import AddPropForm from './addPropForm.jsx';
 
@@ -34,7 +33,6 @@ class PropertiesContainer extends React.Component {
     this.state = {
       addProp: false
     };
-
     this.openHandler = this.openHandler.bind(this);
   }
 
@@ -42,22 +40,32 @@ class PropertiesContainer extends React.Component {
     this.setState({addProp: !this.state.addProp});
   }
 
-  componentDidMount() {
-
-  }
-
+  // removes property from redux/db
   deleteProperty(property) {
     this.props.dispatch(deleteProperty(property));
+  }
+
+  // gets QR Code handling check-in of that property
+  handleGenerateQR(property) {
+    axios.get('/qrCode', {responseType: 'arraybuffer'})
+    .then((res) => {
+      var arr = new Uint8Array(res.data);
+      var raw = String.fromCharCode.apply(null, arr);
+      var b64 = btoa(raw);
+      map.src = 'data:image/png;base64,' + b64;
+    })
   }
 
   render() {
     return (
       <div>
         <PropertyList 
-          properties={this.props.properties}deleteProperty={(property) => this.deleteProperty(property)}/>
+          properties={this.props.properties}
+          deleteProperty={(property) => this.deleteProperty(property)}
+          handleGenerateQR={() => this.handleGenerateQR()}/>
         <Card
-        onTouchTap={()=> {this.openHandler()}}
-        style={cardStyle}
+          onTouchTap={()=> {this.openHandler()}}
+          style={cardStyle}
         >
         <RaisedButton fullWidth={true} icon={<AddIcon />} label='Add a Property'/>
         </Card>
--- src/client/app/components/properties/propertyCard.jsx
+++ src/client/app/components/properties/propertyCard.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 const PropertyCard = (props) => {
   return (
     <Card>
-      <CardTitle title={props.property.name} subtitle={props.property.address}>
-      </CardTitle>
+      <CardHeader
+        title={props.property.name} 
+        subtitle={props.property.address}
+        actAsExpander={true}
+        showExpandableButton={true} />
+      <CardActions expandable={true}>
+        <RaisedButton label='Generate QR Code' onTouchTap={props.handleGenerateQR}/>
+        <img id='map'></img>
+      </CardActions>
     </Card>
   );
 }
