c51b3c2b4885c3a94bb7c76661f7e215552cb026
Rob0h <robert.is.oh@gmail.com>
2017-01-11 18:13:01 -0800

    Implement QR code reader with react-qr-reader

--- src/client/app/components/camera/cameraContainer.jsx
+++ src/client/app/components/camera/cameraContainer.jsx
@@ -1,113 +1,43 @@
 import React from 'react';
 import QrCode from 'qrcode-reader';
+import QrReader from 'react-qr-reader'
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
-const previewContainer = {
-  width: '50px',
-  height: '50px'
+const previewStyle = {
+  height: '240px',
+  width: '320px',
 }
 
 class CameraContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.file = undefined;
-  }
-
-  componentDidMount() {
-    var upload = document.getElementById('upload');
-    var preview = document.getElementById('preview');
-    var qr = new QrCode();
-    var context = this;
-
-    qr.callback = function(result, err) {
-      var span = document.querySelector('span') || document.createElement('span');
-      if (result) {
-        span.textContent = result;
-      } else {
-        span.textContent = 'Sorry, the image could not be deciphered. Try again.';
-      }
-      preview.appendChild(span);
+    this.state = {
+      result: 'No result',
     }
-    upload.addEventListener('change', context.handleFileSelect.bind(context), false);
-/*    upload.addEventListener('change', function() {
-      for (var i = 0; i < this.files.length; i++) {
-        var file = this.files[i];
-
-        var imageType = /^image\//;
-            
-        if (!imageType.test(file.type)) {
-          throw new Error('File type not valid');
-        }
-
-        var reader = new FileReader();
-        reader.addEventListener('load', function() {
-          var img = document.querySelector('img') || document.createElement('img');
-          img.src = this.result;
-          preview.appendChild(img);
-
-          qr.decode(this.result);
-        }.bind(reader), false);
-        reader.readAsDataURL(file);
-      }
-    })*/
   }
 
-  handleFileSelect(e) {
-    var file = e.target.files;
-    console.log('file', file);
-    var span = document.createElement('span');
-    span.textContent = file[0].name;
-    document.getElementById('preview').appendChild(span);
-    this.handleSubmit(file);
+  handleScan(data){
+    this.setState({
+      result: data,
+    })
   }
 
-  handleSubmit(file) {
-
-    var upload = document.getElementById('upload');
-    var preview = document.getElementById('preview');
-    var qr = new QrCode();
-
-    qr.callback = function(result, err) {
-      var span = document.querySelector('span') || document.createElement('span');
-      if (result) {
-        span.textContent = result;
-      } else {
-        span.textContent = 'Sorry, the image could not be deciphered. Try again.';
-      }
-      preview.appendChild(span);
-    }
-
-/*    for (var i = 0; i < upload.files.length; i++) {*/
-      //var file = upload.files[i];
-
-      file = file[0];
-      
-      var imageType = /^image\//;
-          
-      if (!imageType.test(file.type)) {
-        throw new Error('File type not valid');
-      }
-
-      var reader = new FileReader();
-      reader.addEventListener('load', function() {
-  /*          var img = document.querySelector('img') || document.createElement('img');
-        img.src = this.result;
-        preview.appendChild(img);*/
-
-        qr.decode(this.result);
-      }.bind(reader), false);
-      reader.readAsDataURL(file);
-/*    }*/
+  handleError(err){
+    console.error(err)
   }
 
   render() {
     return (
-      <Card>
-        <input id='upload' type='file' accept='image/*;capture=camera' />
-        <input type='button' value='submit' onClick={() => this.handleSubmit()}/>
-        <div id='preview' style={ previewContainer }></div>
-      </Card>
+      <div>
+
+        <QrReader
+          previewStyle={previewStyle}
+          handleError={this.handleError}
+          handleScan={this.handleScan.bind(this)}
+          legacyMode={false}/>
+        <p>{this.state.result}</p>
+      </div>
     )
   }
 }
