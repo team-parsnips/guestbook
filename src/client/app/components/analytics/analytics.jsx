85e0fa04ea1a501f044c263f1dbb708a487f00d7
Rob0h <robert.is.oh@gmail.com>
2017-01-18 23:48:26 -0800

    Add ability for graphs to change size based on window size - Rendering is not dynamic

--- src/client/app/components/analytics/analytics.jsx
+++ src/client/app/components/analytics/analytics.jsx
@@ -31,6 +31,8 @@ class Analytics extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      width: 0,
+      height: 0,
       data: this.props.bookings,
       sequenceData: [],
       pieData: []
@@ -38,10 +40,28 @@ class Analytics extends React.Component {
   }
 
   componentWillMount() {
-    // set initial data of sequence and pie with all bookings
+    // set initial height/width
+    this.setGraphSize();
+
+    // set data of sequence/pie with all bookings
     this.setState({
       sequenceData: Utils.bookingMap(this.props.bookings),
       pieData: Utils.bookingPie(this.props.bookings),
+    });
+
+    // add event listener to invoke setGraphSize on window resize events
+    var context = this;
+    window.addEventListener("resize", function() {
+      console.log('invoking setGraphSize');
+      context.setGraphSize();
+      context.forceUpdate();
+    });
+  }
+
+  setGraphSize() {
+    this.setState({
+      height: 0.5 * window.innerHeight,
+      width: 0.67 * window.innerWidth
     });
   }
 
@@ -52,28 +72,28 @@ class Analytics extends React.Component {
           style={cardStyle}>
           <CardTitle title="Monthly Revenue" />
           <CardMedia>
-            <StackedGroupedBar width={250} height={300}/>
+            <StackedGroupedBar width={this.state.width} height={this.state.height}/>
           </CardMedia>
         </Card>
         <Card
           style={cardStyle}>
           <CardTitle title="Visits by Time of Year" />
           <CardMedia>
-            <SequenceSunBurst width={250} height={300} data={this.state.sequenceData} />
+            <SequenceSunBurst width={this.state.width} height={this.state.height} data={this.state.sequenceData} />
           </CardMedia>
         </Card>
         <Card
           style={cardStyle}>
           <CardTitle title="Check Out vs Check In Times" />
           <CardMedia>
-            <ScatterPlot />
+            <ScatterPlot width={this.state.width} height={this.state.height}/>
           </CardMedia>
         </Card>
         <Card
           style={cardStyle}>
           <CardTitle title="Duration of Stay" />
           <CardMedia>
-            <Pie width={250} height={300} data={this.state.pieData} group='durationOfStay' count='freq'/>
+            <Pie width={this.state.width} height={this.state.height} data={this.state.pieData} group='durationOfStay' count='freq'/>
           </CardMedia>
         </Card>
       </div>
