fabb991c00addbc8434a06fe0e7c7569e81498fd
Rob0h <robert.is.oh@gmail.com>
2017-01-18 16:54:02 -0800

    Increase prediction-service request timeout

--- src/server/controllers/predictionController.js
+++ src/server/controllers/predictionController.js
@@ -2,9 +2,22 @@ var axios = require('axios');
 
 module.exports = {
   predictPrice: function(req, res, next) {
-    axios.post('http://localhost:5000', {
-      instant_book: true,
-      satisfaction_guest: 98
+    axios({
+      method: 'post',
+      url: 'http://localhost:5000',
+      timeout: 600000,
+      data: {
+        location: 'irvine',
+        instant_book: 1,
+        satisfaction_guest: 98,
+        person_capacity: 2,
+        rating_communication: 10,
+        rev_count: 214,
+        cancel_policy: 4,
+        rating_cleanliness: 10,
+        response_rate: 0.958, 
+        rating_checkin: 10,
+      }
     })
     .then(function(predicted) {
       console.log(predicted.data);
